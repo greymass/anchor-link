805662d65b92f1ea2234280067098958a69b7b19
Johan Nordberg <its@johan-nordberg.com>
2020-12-04 17:20:16 +0900

    Update bundler

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,14 @@
 export * from './link'
 export * from './link-session'
-export {LinkOptions} from './link-options'
-export {LinkTransport} from './link-transport'
-export {LinkStorage} from './link-storage'
+export type {LinkOptions} from './link-options'
+export type {LinkTransport} from './link-transport'
+export type {LinkStorage} from './link-storage'
 export * from './errors'
 
 // default export is Link class for convenience
 import {Link} from './link'
 export default Link
 
-// convenience re-exports from esr
-export {PlaceholderAuth, PlaceholderName, PlaceholderPermission} from 'eosio-signing-request'
+// expose dependencies
+export * from 'eosio-signing-request'
+export * from '@greymass/eosio'
