fffef8b6d38092f950c30d6453c0f6463acbbbb5
Johan Nordberg <its@johan-nordberg.com>
2021-02-12 17:20:27 +0900

    Don't infer version string type

--- src/link.ts
+++ src/link.ts
@@ -181,7 +181,7 @@ export class LinkChain implements AbiProvider {
  */
 export class Link {
     /** Package version. */
-    static version = '__ver' // replaced by build script
+    static version: string = '__ver' // eslint-disable-line @typescript-eslint/no-inferrable-types
 
     /** Chains this instance is configured with. */
     public readonly chains: LinkChain[]
